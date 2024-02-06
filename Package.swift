2706f3bee7ddca37d2cdf116341293117b7e3802
Monsoir <raeyeung.mon@gmail.com>
2024-02-06 21:27:11 +0800

    update: update binary target links

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let version = "v1.0.1"
+let version = "v1.0.2"
 
 let package = Package(
     name: "FFmpeg-iOS",
@@ -19,14 +19,14 @@ let package = Package(
         .package(url: "https://github.com/kewlbear/FFmpeg-iOS-Support", from: "0.0.1"),
     ],
     targets: [
-        .binaryTarget(name: "avcodec", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avcodec.xcframework.zip", checksum: "fd36820089a60a852392ed945a64fb52975e652249511b50b1203cd52adf8a98"),
-        .binaryTarget(name: "avutil", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avutil.xcframework.zip", checksum: "5360e5ac16beebb8b8b0299233947eda9aa630d90a885531d80616f5b1cb892a"),
-        .binaryTarget(name: "avformat", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avformat.xcframework.zip", checksum: "ee157fac4c1aaf72c8e8218ec5f9771fc926fe33ac02171eb49cd648e7d4ad38"),
-        .binaryTarget(name: "avfilter", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avfilter.xcframework.zip", checksum: "f57c57cb6f70411443a8ecda171f9de14e415e92293facc3f402cd1de212e520"),
-        .binaryTarget(name: "avdevice", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avdevice.xcframework.zip", checksum: "5ec8ac964f4d99b081111e17bbff129534f87d135a1bc7861c8773a744e16823"),
-        .binaryTarget(name: "swscale", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/swscale.xcframework.zip", checksum: "527542f997f0915d8912eb0acb8f59f87b4086c06f2427847f5b6ce7bb32b301"),
-        .binaryTarget(name: "swresample", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/swresample.xcframework.zip", checksum: "baf0a70780785a6edb6c9e179f11bb28ec934027625a14dbd1f37c896c878ac1"),
-        .binaryTarget(name: "fftools", url: "https://github.com/kewlbear/FFmpeg-iOS/releases/download/v0.0.6-b20230416-173821/fftools.zip", checksum: "da3e909deb1ee98ef2425a2a4640b663fbb84ecc4204d663b1959c8de587acc7"),
+        .binaryTarget(name: "avcodec", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avcodec.zip", checksum: "af84e002806200e43c39940b9f52d1e4d432bf74ab11ed2fd297e6e743a376aa"),
+        .binaryTarget(name: "avutil", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avutil.zip", checksum: "fc121ea6f6db8bffa6f14527201ef12111bc0ee347d0a4739f0a973e862291d8"),
+        .binaryTarget(name: "avformat", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avformat.zip", checksum: "d25c53a75fcb67818c3843df82b767318af147caae0060597362bb2b5b4b352b"),
+        .binaryTarget(name: "avfilter", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avfilter.zip", checksum: "c6e99e7ab4555690d87403f07a6f92f1952251399ef5cc490657e3e17d802fd6"),
+        .binaryTarget(name: "avdevice", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/avdevice.zip", checksum: "bada0751e28cebc8d15dc153ff09b6b8b456da3cf419cf7b14f9804b93b1bbcb"),
+        .binaryTarget(name: "swscale", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/swscale.zip", checksum: "232daeb254ebaaa3f16c9263a1b38230c16ccf1e746dbb21e054b594c84312eb"),
+        .binaryTarget(name: "swresample", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/swresample.zip", checksum: "902469fe2314c37b407ea3d123efe49c004c7e2182b9ab7c8405bfc8ba44a14d"),
+        .binaryTarget(name: "fftools", url: "https://github.com/monsoir/FFmpeg-iOS/releases/download/\(version)/fftools.zip", checksum: "9a8d52c71e1c8f0dc21e65da307a459fa3006a3b79b4917f795906fb9138803a"),
         .target(name: "Dummy", dependencies: [
             "fftools",
             "avcodec", "avformat", "avfilter", "avdevice", "avutil", "swscale", "swresample",
